0c15f20a0dd333a80aa5b8385235475fbf882f58
jamieastley <6836556+jamieastley@users.noreply.github.com>
2023-02-04 19:32:16 +1100

    and now delete example to move out

--- example/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "aws_ami" {
-  description = "The AMI ID of the created EC2 instance"
-  value       = module.Nginx_Demo.aws_ami
-}
-
-output "elastic_ip" {
-    value = module.Nginx_Demo.elastic_ip
-}
