d9b55d0b0d6f8af526103d31fea6630a7b9dc870
jamieastley <6836556+jamieastley@users.noreply.github.com>
2024-12-08 15:16:19 +1100

    update Cloudflare record

--- example/main.tf
+++ example/main.tf
@@ -6,13 +6,14 @@ terraform {
     }
 
     cloudflare = {
-      source = "cloudflare/cloudflare"
-      version = "~> 4.47.0"
+      source  = "cloudflare/cloudflare"
+      version = ">= 4.47"
     }
   }
 }
 
 locals {
+  app_name                     = "example-server"
   docker_compose_file          = "docker-compose.yml"
   docker_compose_template_path = "templates/docker-compose.tftpl"
   docker_compose_s3_key_path   = "scripts/${local.docker_compose_file}"
@@ -44,15 +45,15 @@ resource "aws_s3_object" "docker_compose" {
 
 module "module_example" {
   source            = "../"
-  app_name          = "hello-world"
+  app_name          = local.app_name
+  app_description   = "Example app to demo the module with"
   aws_ami           = "ami-0e040c48614ad1327"
   aws_instance_type = "t2.micro"
-  s3_bucket_id      = aws_s3_bucket.example_bucket.id
   ssh_key_name      = "id_dev"
   instance_user_data = templatefile(local.init_ec2_template_path, {
     bucket                     = aws_s3_bucket.example_bucket.bucket
     docker_compose_s3_key_path = local.docker_compose_s3_key_path
-    app_name                   = "hello-world"
+    app_name                   = local.app_name
     docker_compose_file        = local.docker_compose_file
     username                   = "ubuntu"
   })
@@ -62,11 +63,11 @@ module "module_example" {
   ingress_rules = flatten([
     [
       {
-        description = "HTTP traffic"
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        description      = "HTTP traffic"
+        from_port        = 80
+        to_port          = 80
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
         ipv6_cidr_blocks = ["::/0"]
       },
       {
@@ -77,33 +78,33 @@ module "module_example" {
         cidr_blocks = ["0.0.0.0/0"]
       },
     ],
-      length(data.http.dev_outbound_ip) != 0 ? [
+    length(data.http.dev_outbound_ip) != 0 ? [
       {
-        description = "Allow SSH connections only from the IP address of the developer"
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = ["${chomp(data.http.dev_outbound_ip[0].response_body)}/32"]
+        description      = "Allow SSH connections only from the IP address of the developer"
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["${chomp(data.http.dev_outbound_ip[0].response_body)}/32"]
         ipv6_cidr_blocks = ["::/0"]
       }
     ] : []
   ])
   egress_rules = [
     {
-      description = "Outbound HTTP"
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+      description      = "Outbound HTTP"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
     }
   ]
 }
 
-resource cloudflare_record "record" {
+resource "cloudflare_record" "record" {
   zone_id = var.cloudflare_zone_id
-  name = "example"
-  type = "A"
+  name    = local.app_name
+  type    = "A"
   proxied = true
-  value = module.module_example.elastic_ip
+  value   = module.module_example.elastic_ip
 }
