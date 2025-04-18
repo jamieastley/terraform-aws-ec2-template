b201da63e038f2d9dd983ea592d653421868b8a3
jamieastley <6836556+jamieastley@users.noreply.github.com>
2025-04-18 16:19:03 +1000

    update example to implement ec2_public_key

--- example/variables.tf
+++ example/variables.tf
@@ -8,3 +8,10 @@ variable "cloudflare_zone_id" {
   type        = string
   description = "The Cloudflare zone ID"
 }
+
+variable "ec2_public_key" {
+  description = "The public key to use for the EC2 instance"
+  type        = string
+  nullable    = false
+  sensitive   = true
+}
